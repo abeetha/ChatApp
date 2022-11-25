26176cde78b5aef452cab20f2699bdf0b1446734
Abeetha <abeethaabeysundera@gmail.com>
2022-11-25 18:51:13 +0530

    Initial commit

---
+++ src/com/seekerscloud/chat_app/controller/LoginFormController.java
@@ -0,0 +1,31 @@
+package com.seekerscloud.chat_app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public AnchorPane loginFormContext;
+
+    public void startChatOnAction(ActionEvent actionEvent) throws IOException {
+        if(!txtUserName.getText().trim().isEmpty()){
+          Stage st = (Stage) loginFormContext.getScene().getWindow();
+          st.setTitle(txtUserName.getText());
+          FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/clientForm.fxml"));
+          Parent parent = fxmlLoader.load();
+          ClientFormController controller = fxmlLoader.getController();
+          controller.setClientName(txtUserName.getText());
+          st.setScene(new Scene(parent));
+        }else{
+            new Alert(Alert.AlertType.WARNING,"User name is required!").show();
+        }
+    }
+}
---
+++ src/com/seekerscloud/chat_app/controller/ServerFormController.java
@@ -0,0 +1,26 @@
+package com.seekerscloud.chat_app.controller;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ServerFormController {
+
+    public void initialize(){
+        new Thread(()->{
+            try{
+                ServerSocket serverSocket = new ServerSocket(5000);
+                System.out.println("Server Started");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public TextField txtMessageBox;
+
+    public void shutdownServerOnClick(MouseEvent mouseEvent) {
+    }
+}
